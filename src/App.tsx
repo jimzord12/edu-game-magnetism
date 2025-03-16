5ed8e606ccac60ad13369e7f34a83c104fad9aa4
Dimitrios Stamatakis <45168324+jimzord12@users.noreply.github.com>
2025-03-16 10:09:41 +0200

    Integrated p5 + matterjs | Working

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useRef } from 'react';
+import p5 from 'p5';
+import { Bodies, Engine, Mouse, MouseConstraint, World } from 'matter-js';
+import type { Body } from 'matter-js';
+import Matter from 'matter-js';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const canvasRef = useRef<HTMLDivElement>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  useEffect(() => {
+    if (canvasRef.current === null) return;
+    // p5's Global Scope
 
-export default App
+    const sketch = (p: p5) => {
+      let boxA: InstanceType<typeof Body>,
+        boxB: InstanceType<typeof Body>,
+        ground: InstanceType<typeof Body>;
+
+      let engine: Engine;
+      let world: World;
+      let mouse: Mouse;
+      let mouseConstraint: MouseConstraint;
+
+      p.setup = () => {
+        p.createCanvas(800, 400);
+
+        engine = Matter.Engine.create();
+        world = engine.world;
+
+        mouse = Mouse.create(canvasRef.current!);
+        mouseConstraint = MouseConstraint.create(engine, {
+          mouse: mouse,
+        });
+
+        boxA = Bodies.rectangle(200, 200, 50, 50);
+        boxB = Bodies.rectangle(300, 50, 80, 80);
+        ground = Bodies.rectangle(400, 380, 810, 20, { isStatic: true });
+
+        World.add(world, [boxA, boxB, ground, mouseConstraint]);
+      };
+
+      p.draw = () => {
+        Engine.update(engine);
+        p.background(220);
+
+        p.rectMode(p.CENTER);
+        p.fill(127);
+        p.rect(boxA.position.x, boxA.position.y, 50, 50);
+        p.rect(boxB.position.x, boxB.position.y, 80, 80);
+
+        p.fill(0);
+        p.rect(ground.position.x, ground.position.y, 810, 20);
+      };
+    };
+
+    const p5Instance = new p5(sketch, canvasRef.current!);
+    return () => p5Instance.remove();
+  }, [canvasRef.current]);
+
+  return <div ref={canvasRef} />;
+};
+
+export default App;
