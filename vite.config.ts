4d78fc84754d0084d2385c42c44a30e4dd530d1e
Dimitrios Stamatakis <45168324+jimzord12@users.noreply.github.com>
2025-03-30 18:54:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -6,21 +6,25 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: `/${import.meta.env.GITHUB_REPO_NAME}/`, // Access via import.meta.env
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@features': path.resolve(__dirname, 'src/features'),
-      '@models': path.resolve(__dirname, 'src/models'),
-      '@config': path.resolve(__dirname, 'src/config'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@router': path.resolve(__dirname, 'src/router'),
-      '@services': path.resolve(__dirname, 'src/services'),
-      '@store': path.resolve(__dirname, 'src/store'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    base: `/${env.GITHUB_REPO_NAME}/`,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+        '@features': path.resolve(__dirname, 'src/features'),
+        '@models': path.resolve(__dirname, 'src/models'),
+        '@config': path.resolve(__dirname, 'src/config'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@router': path.resolve(__dirname, 'src/router'),
+        '@services': path.resolve(__dirname, 'src/services'),
+        '@store': path.resolve(__dirname, 'src/store'),
+      },
     },
-  },
+  };
 });
