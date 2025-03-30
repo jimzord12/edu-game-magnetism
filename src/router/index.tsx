ccd475992ead1398369cc6d465a0cfd014c9702d
Dimitrios Stamatakis <45168324+jimzord12@users.noreply.github.com>
2025-03-30 18:33:14 +0300

    Fixes #5, Replaced BrowserRouter with HashRouter to work with GH-Pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,35 +1,19 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import LevelSelectPage from '../pages/LevelSelectPage';
 import GamePage from '../pages/GamePage';
 import SandboxPage from '../pages/SandboxPage';
 import { routePath } from './routePath';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <HomePage />,
-    },
-    {
-      path: routePath.levelSelect,
-      element: <LevelSelectPage />,
-    },
-    {
-      path: routePath.game,
-      element: <GamePage />,
-    },
-
-    {
-      path: routePath.sandbox,
-      element: <SandboxPage />,
-    },
-  ],
-  {
-    basename: '/edu-game-magnetism', // for Github Pages deployment
-  }
-);
-
 export const AppRouter: React.FC = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path={routePath.levelSelect} element={<LevelSelectPage />} />
+        <Route path={routePath.game} element={<GamePage />} />
+        <Route path={routePath.sandbox} element={<SandboxPage />} />
+      </Routes>
+    </HashRouter>
+  );
 };
