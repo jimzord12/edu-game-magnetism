09eba570c845ec1d0d1821bd5f897e45fd81517e
Dimitrios Stamatakis <45168324+jimzord12@users.noreply.github.com>
2025-03-30 17:36:11 +0300

    Prep for GitHub Page Deployment throught GH-Actions

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,25 +5,30 @@ import GamePage from '../pages/GamePage';
 import SandboxPage from '../pages/SandboxPage';
 import { routePath } from './routePath';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: routePath.levelSelect,
-    element: <LevelSelectPage />,
-  },
-  {
-    path: routePath.game,
-    element: <GamePage />,
-  },
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: routePath.levelSelect,
+      element: <LevelSelectPage />,
+    },
+    {
+      path: routePath.game,
+      element: <GamePage />,
+    },
 
+    {
+      path: routePath.sandbox,
+      element: <SandboxPage />,
+    },
+  ],
   {
-    path: routePath.sandbox,
-    element: <SandboxPage />,
-  },
-]);
+    basename: '/edu-game-magnetism', // for Github Pages deployment
+  }
+);
 
 export const AppRouter: React.FC = () => {
   return <RouterProvider router={router} />;
