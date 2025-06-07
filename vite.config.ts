6a0be01618b20dca82343d599c4e43de9f9885f3
Dimitrios Stamatakis <dev1@ics.gr>
2025-06-07 07:55:33 +0300

    Changed Vite config to allow building files for local serving

--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,40 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig({
+  // const env = loadEnv(mode, process.cwd(), '');
 
-  return {
-    plugins: [react(), tailwindcss()],
-    base: `/${env.GITHUB_REPO_NAME}/`,
-    optimizeDeps: {
-      exclude: ['sqlocal'],
+  plugins: [react(), tailwindcss()],
+  base: `/`,
+  // base: `/${env.GITHUB_REPO_NAME}/`,
+  optimizeDeps: {
+    exclude: ['sqlocal'],
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
     },
-    server: {
-      headers: {
-        'Cross-Origin-Opener-Policy': 'same-origin',
-        'Cross-Origin-Embedder-Policy': 'require-corp',
-      },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@features': path.resolve(__dirname, 'src/features'),
+      '@models': path.resolve(__dirname, 'src/models'),
+      '@config': path.resolve(__dirname, 'src/config'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@router': path.resolve(__dirname, 'src/router'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@store': path.resolve(__dirname, 'src/store'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@db': path.resolve(__dirname, 'src/db'),
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-        '@components': path.resolve(__dirname, 'src/components'),
-        '@hooks': path.resolve(__dirname, 'src/hooks'),
-        '@features': path.resolve(__dirname, 'src/features'),
-        '@models': path.resolve(__dirname, 'src/models'),
-        '@config': path.resolve(__dirname, 'src/config'),
-        '@pages': path.resolve(__dirname, 'src/pages'),
-        '@router': path.resolve(__dirname, 'src/router'),
-        '@services': path.resolve(__dirname, 'src/services'),
-        '@store': path.resolve(__dirname, 'src/store'),
-        '@assets': path.resolve(__dirname, 'src/assets'),
-        '@db': path.resolve(__dirname, 'src/db'),
-      },
-    },
-    build: {
-      rollupOptions: {
-        output: {
-          external: ['sqlocal'], // 👈 skip bundling sqlocal
-          manualChunks: undefined,
-        },
-      },
-    },
-    test: {
-      environment: 'jsdom',
-      globals: true,
-      setupFiles: './src/setupTests.ts',
-      include: ['**/*.{test,spec}.{ts,tsx}'],
-    },
-    worker: {
-      format: 'es',
-    },
-  };
+  },
+  worker: {
+    format: 'es',
+  },
 });
